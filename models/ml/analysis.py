dff8557961439bc1b9405ee0d14ddf48d2f49d4f
Vivek Mehendiratta <mehendiratta.vivek@gmail.com>
2022-05-27 00:13:42 -0500

    cml_test

--- models/ml/analysis.py
+++ models/ml/analysis.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from models.ml.training import transform_data, build_model, evaluate_model
+from sklearn.metrics import ConfusionMatrixDisplay, PrecisionRecallDisplay
 import numpy as np
+from matplotlib import pyplot as plt
+
 
 path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz'
 data = pd.read_csv(path, header=None)
@@ -21,3 +24,21 @@ scores_dict = {"Precision": round(np.mean(scores['test_precision']), 2),
 
 print(scores_dict)
 
+## metrics report
+with open("metrics.txt", 'w') as f: 
+    for key, value in scores_dict.items(): 
+        f.write('%s:%s\n' % (key, value))
+
+
+# confusion matrix
+ConfusionMatrixDisplay.from_estimator(
+    pipe, X, y, cmap="YlGn", normalize="true"
+)
+plt.savefig('CM.png')
+
+# PR curve
+display = PrecisionRecallDisplay.from_estimator(
+    pipe, X, y, name="XGBoost"
+)
+_ = display.ax_.set_title("2-class Precision-Recall curve")
+plt.savefig("PR_curve.png")
