908972712818a1eb278408b15ef3821374f51cbe
Vivek Mehendiratta <mehendiratta.vivek@gmail.com>
2022-05-28 00:22:34 -0500

    port hard code removed

--- routes/home.py
+++ routes/home.py
@@ -3,6 +3,6 @@ from starlette.responses import RedirectResponse
 app_home = APIRouter()
 
 
-@app_home.get('/home', tags=["Welcome to Census Income Prediction Page"])
+@app_home.get('/', tags=["Welcome to Census Income Prediction Page"])
 async def hello():
     return RedirectResponse("/redoc")
